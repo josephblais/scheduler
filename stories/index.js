65d1c34df2e10cbb857b9d749a3c5c4ae625eedb
Joseph Blais <josephpierreblais@gmail.com>
2021-03-16 19:55:39 -0400

    Add further Appointment stories

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,8 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 
 storiesOf("Button", module)
@@ -144,3 +146,5 @@ storiesOf("Appointment", module)
   .add("Header", () => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Confirm", () => <Confirm message="Delete-a this right-a now?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
+  .add("Status", () => <Status message="I'LL DELETA THAT FOR U" />)
